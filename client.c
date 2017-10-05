32b86960aec9621aec9a63c50de771068e8f6833
root <root@shartzard.lol>
2017-10-05 15:56:46 -0700

    organized client src code for better readability

--- client.c
+++ client.c
@@ -9,48 +9,50 @@
 
 #define BUFSIZE 512
 
+char DEFAULT_HOST[] = "127.0.0.1";
+int DEFAULT_PORT = 4444;
+
+int sockfd, portno;
+struct sockaddr_in serveraddr;
+struct hostent *server;
+char *hostname;
+
 void error(char *msg) {
 	perror(msg);
 	exit(0);
 }
 
-int main(int argc, char **argv) {
-	int sockfd, portno, n;
-	int serverlen;
-	struct sockaddr_in serveraddr;
-	struct hostent *server;
-	char *hostname;
-	char buf[BUFSIZE];
-	
-	char ENTER_CHAT[] = "enter";
+void resolve_host() {
 
-    	/* check command line arguments */
-	if (argc != 3) {
-		fprintf(stderr,"usage: %s <hostname> <port>\n", argv[0]);
-		exit(0);
-	}
-
-	hostname = argv[1];
-	portno = atoi(argv[2]);
 	memset((char *)&serveraddr, 0, sizeof(serveraddr));
-	memset(buf, 0, BUFSIZE);
 
-    	/* socket: create the socket */
+    	/* create the socket */
 	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 	if (sockfd < 0) 
 		error("ERROR opening socket");
 
-  	/* gethostbyname: get the server's DNS entry */
+  	/* get the server's DNS entry */
 	server = gethostbyname(hostname);
 	if (server == NULL) {
 		fprintf(stderr,"ERROR, no such host as %s\n", hostname);
 		exit(0);
 	}
 
-	/* build the server's Internet address */
 	serveraddr.sin_family = AF_INET;
 	bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
 	serveraddr.sin_port = htons(portno);
+	
+	return;
+}
+
+void init_server_connection() {
+	int n, serverlen;
+	char buf[BUFSIZE];
+	char ENTER_CHAT[] = "enter";
+
+	memset(buf, 0, BUFSIZE);
+
+	resolve_host();
 
 	/* send the message to the server */
 	serverlen = sizeof(serveraddr);
@@ -74,5 +76,70 @@ int main(int argc, char **argv) {
 		error("ERROR: sendto failed");
 
 	close(sockfd);
+	return;
+}
+
+int main(int argc, char **argv) {
+	/*
+	int sockfd, portno, n;
+	int serverlen;
+	struct sockaddr_in serveraddr;
+	struct hostent *server;
+	char *hostname;
+	char buf[BUFSIZE];
+	*/	
+
+    	/* check command line arguments */
+	if (argc != 3) {
+		hostname = DEFAULT_HOST;
+		portno = DEFAULT_PORT;
+	}else{
+		hostname = argv[1];
+		portno = atoi(argv[2]);
+	}
+
+	init_server_connection();
+
+	/*
+	memset((char *)&serveraddr, 0, sizeof(serveraddr));
+	memset(buf, 0, BUFSIZE);
+ 
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0) 
+		error("ERROR opening socket");
+	
+	server = gethostbyname(hostname);
+	if (server == NULL) {
+		fprintf(stderr,"ERROR, no such host as %s\n", hostname);
+		exit(0);
+	}
+
+	serveraddr.sin_family = AF_INET;
+	bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+	serveraddr.sin_port = htons(portno);
+
+
+	serverlen = sizeof(serveraddr);
+	n = sendto(sockfd, ENTER_CHAT, 5, 0, (struct sockaddr *)&serveraddr, serverlen);
+	if (n < 0) 
+		error("ERROR in sendto");
+     
+	n = recvfrom(sockfd, buf, BUFSIZE, 0, (struct sockaddr *)&serveraddr, &serverlen);
+	if (n < 0) 
+		error("ERROR in recvfrom");
+	printf("Server requested to use port# %s", buf);
+	portno = atoi(buf);
+	serveraddr.sin_port = htons(portno);
+
+	memset(buf, 0, BUFSIZE);
+	printf("Please enter msg: ");
+	fgets(buf, BUFSIZE, stdin);
+
+	n = sendto(sockfd, buf, strlen(buf), 0, (struct sockaddr *)&serveraddr, serverlen);
+	if(n<0)
+		error("ERROR: sendto failed");
+
+	close(sockfd);
+	*/
 	return 0;
 }
