34d283b4ecf127891cfd4080570fd9bef02621e1
root <root@shartzard.lol>
2017-10-24 17:58:33 -0700

    added prompt and input parsing to client

--- client.c
+++ client.c
@@ -19,6 +19,13 @@ const uint32_t _IN_WHO = 6;
 const uint32_t _IN_LIVE = 7;
 const uint32_t _IN_ERROR = 99;
 
+const char _CMD_EXIT[]="exit";
+const char _CMD_JOIN[]="join";
+const char _CMD_LEAVE[]="leave";
+const char _CMD_LIST[]="list";
+const char _CMD_WHO[]="who";
+const char _CMD_SWITCH[]="switch";
+
 char DEFAULT_HOST[] = "127.0.0.1";
 int DEFAULT_PORT = 4444;
 
@@ -52,6 +59,125 @@ void resolve_host() {
 	bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
 	serveraddr.sin_port = htons(portno);
 	
+	return;
+}
+
+void build_request(int argc, char **argv){
+	int i;
+
+	if(memcmp(argv[0], _CMD_EXIT, strlen(_CMD_EXIT)) == 0){
+		//build_request(argc, argv);
+
+	}else if(memcmp(argv[0], _CMD_JOIN, strlen(_CMD_JOIN)) == 0){
+		for(i=0; i<argc; i++){
+                	printf("DEBUG:\nargv[%d]: %s\n", i, argv[i]);
+        	}
+
+	}else if(memcmp(argv[0], _CMD_LEAVE, strlen(_CMD_LEAVE)) == 0){
+		//build_request(argc, argv);
+
+	}else if(memcmp(argv[0], _CMD_LIST, strlen(_CMD_LIST)) == 0){
+		//build_request(argc, argv);
+
+	}else if(memcmp(argv[0], _CMD_WHO, strlen(_CMD_WHO)) == 0){
+		//build_request(argc, argv);
+
+	}else if(memcmp(argv[0], _CMD_SWITCH, strlen(_CMD_SWITCH)) == 0){
+		//build_request(argc, argv);
+
+	}else{
+		printf("Command '%s' not recognized.\n", argv[0]);
+	}
+
+	return;
+}
+
+void resolve_cmd(char * input){
+	char cmd[BUFSIZE+1];
+	char **argv;
+	int i, offset, n;
+	int argc = 0;
+
+	memset(cmd, 0, BUFSIZE+1);
+
+	for(i=1; i<BUFSIZE; i++){
+		//uses i-1 to dispense of '/' char
+		if(input[i] < 0x21 || input[i] > 0x7e){ 
+			cmd[i-1] = 0x0;
+		}else{
+			cmd[i-1] = input[i];
+		}
+	}
+
+	i=0;
+	n=0;
+	while(i<BUFSIZE+1){
+		n = strlen(&cmd[i]);
+		if(n < 1){
+			i++;
+			continue;
+		}
+		i+=n+1;
+		argc++;
+	}
+	printf("DEBUG: argc = %d\n", argc);
+
+	argv = malloc(sizeof(char *) * argc);
+	if(!argv)
+		error("ERROR: in resolve_cmd() failed to allocate argv");
+
+	offset = 0;
+	for(i=0; i<argc; i++){	
+		n = strlen(&cmd[offset]);
+		if(n < 1)
+			error("ERROR: received command arg with invalid size");
+
+		argv[i] = malloc(n+1);
+		if(!argv[i])
+			error("ERROR: in resolve_cmd() failed to allocate argc");
+
+		memset(argv[i], 0, n+1);
+		strcpy(argv[i], &cmd[offset]);
+		offset+=n+1;
+		if(offset > BUFSIZE+1)
+			error("ERROR: offset ran out of buffer bounds");	
+	}
+
+	build_request(argc, argv);
+
+	for(i=0; i<argc; i++){
+		free(argv[i]);
+	}
+	free(argv);
+
+	return;
+}
+
+void user_prompt(){
+	char *input;
+	int n;
+
+	input = (char *)malloc(BUFSIZE);
+	if(!input)
+		error("ERROR: malloc returned null in user_prompt()");
+
+	while(1){
+		memset(input, 0, BUFSIZE);
+		printf("> ");
+		if(fgets(input, BUFSIZE, stdin) == NULL)
+			continue;
+
+		if(input[0] == 0x2f){
+			resolve_cmd(input);
+		}else if(input[0] == 0x2e){
+			break;
+		}else{
+			printf("Sending message: %s\n", input);
+		}
+	}
+
+	free(input);
+
 	return;
 }
 
@@ -138,7 +264,8 @@ int main(int argc, char **argv) {
 		portno = atoi(argv[2]);
 	}
 
-	init_server_connection();
+	//init_server_connection();
+	user_prompt();
 
 	return 0;
 }
